cca1647f8a9f02c14e8a094daa60592f9aacd605
pkpirhonen <pkpirhonen@gmail.com>
2019-08-13 09:48:30 +0300

    calendarcontainer.jsx updated and calendarDayRow.jsx's first draft.

---
+++ CalenderApp/CalenderApp/Client/app.calendarDayRow.jsx
@@ -0,0 +1,22 @@
+﻿import React, { Component } from 'react';
+
+
+export class CalendarDayRow extends Component {
+
+    constructor(props) {
+        super(props);
+        this.dateObj = this.props.dateObj;
+        this.eventCat = this.props.eventCat;
+    }
+
+    render() {
+
+        return <div>
+
+            <div>{this.dateObj.weekday}</div>
+            <div>{this.dateObj.day}</div>
+            <div>{this.dateObj.month}</div>
+            <div>{this.dateObj.year}</div>
+        </div>
+    }
+}
--- CalenderApp/CalenderApp/Client/app.calendarcontainer.jsx
+++ CalenderApp/CalenderApp/Client/app.calendarcontainer.jsx
@@ -1,11 +1,35 @@
 ﻿import React, { Component } from 'react';
 import { HTTP } from './http';
+import { CalendarDayRow } from './app.calendarDayRow';
 
+// newDate -> fetch eventData -> store it -> put it to template -> set nextDate
 
 export class CalendarContainer extends Component {
 
     constructor(props) {
         super(props);
+
+        // dateObj[] contains weekday, dayOfMonth, Month
+
+        this.date = new Date();
+        this.arrDateObj = [];
+
+        /*
+        this.dateObj = {
+            weekday: "Sunnuntai",
+            dayOfMonth: 11,
+            month: "elokuu",
+            year: 2019
+        }
+        this.arrDateObj.push(this.dateObj);
+        this.arrDateObj.push({
+            weekday: "Maanantai",
+            dayOfMonth: 12,
+            month: "elokuu",
+            year: 2019
+        });
+        */
+
         this.weekday = ["Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai", "Sunnuntai"];
 
         this.eventData = {
@@ -29,22 +53,80 @@ export class CalendarContainer extends Component {
 
         this.eventDataMap = new Map();
         let category = this.eventData['Category'];
-        let date = this.eventData['BeginningDateTime'].slice(0, 9); //???
-        console.log(date);
+        // let date = this.eventData['BeginningDateTime'].slice(0, 10); //???
+        // console.log(date);
         let tmpArr = [];
         tmpArr.push(this.eventData);
         //this.eventDateMap.set(date, this.eventDataCat[category].push(this.eventData)); // kaikki data tänne talteen
    
     }
 
+   
     render() {
 
-        let elemCal = this.weekday.map((day) => <div>{day}</div>);
+        function calendarDayFormat(dateToFormat) {
+            let options = {
+                weekday: "long",
+                year: "numeric",
+                month: "long",
+                day: "numeric"
+            };
+            let arrDate = dateToFormat.toLocaleDateString("fi", options).split(" ");
+            let dateObj2 = {};
+            dateObj2.weekday = arrDate[0].slice(0, (arrDate[0].length - 2));
+            dateObj2.day = dateToFormat.getDate();
+            dateObj2.month = arrDate[2].slice(0, (arrDate[2].length - 2));
+            dateObj2.year = dateToFormat.getFullYear();
+            return dateObj2;
+        }
+
+        
+        function createCalDayArray(d) {
+            let daysInWeek = 7
+            let tmpArrDate = [];
+            tmpArrDate.push(calendarDayFormat(d));
+            let nextDay = new Date(d);
+            nextDay.setDate(d.getDate() + 1);
+            console.log('Huominen: ' + nextDay);
+            tmpArrDate.push(calendarDayFormat(nextDay));
+            while (tmpArrDate.length < daysInWeek) {
+                nextDay.setDate(nextDay.getDate() + 1);
+                console.log('Ylihuominen: ' + nextDay);
+                tmpArrDate.push(calendarDayFormat(nextDay));
+            }
+            return tmpArrDate;
+        }
+        
+
+        let d = new Date();
+        this.arrDateObj = createCalDayArray(d);
+        console.log('Tulostetaan originaali: ' + d.getDate());
+        console.log('Tulostetaan lokalisoitu: ' + calendarDayFormat(d).weekday + ', ' + calendarDayFormat(d).day + ', '+
+            calendarDayFormat(d).month + ', ' + calendarDayFormat(d).year );
+
         return <div>
-                <div className="weekday">
-                    Kalenteri11
-                    {elemCal}
+                    
+                    <div className="containerFirstRow">
+                        <div className="calendar">
+                    <CalendarDayRow dateObj={this.arrDateObj[0]} eventCat={this.eventDataCat} />
+                            <br /> 
+                            <CalendarDayRow dateObj={this.arrDateObj[1]} />
+                            <br />
+                            <CalendarDayRow dateObj={this.arrDateObj[2]} />
+                            <br />
+                            <CalendarDayRow dateObj={this.arrDateObj[3]} />
+                            <br />
+                            <CalendarDayRow dateObj={this.arrDateObj[4]} />
+                            <br />
+                            <CalendarDayRow dateObj={this.arrDateObj[5]} />
+                            <br />
+                            <CalendarDayRow dateObj={this.arrDateObj[6]} />
+                        </div>                
+                    </div>
+                    <br />
+                    <br />
+                    <div>Kalenteri16</div> 
+
                 </div>
-            </div>
     }
 }
